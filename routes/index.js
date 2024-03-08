8c6b37a9570a88a959e8451ba48a585a238db87e
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-03-08 18:04:34 +0530

    player added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/currentVideo', function (req, res, next) {
+  res.render('currentVideo')
+})
 
 module.exports = router;
