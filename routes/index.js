e7fa5429805ac1461fdd6827eb49b7cdf1309a1b
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-03-12 13:23:36 +0530

    login and register page added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res, next) => {
+  res.render('register')
+})
+
+
 
 router.get('/currentVideo', function (req, res, next) {
   res.render('currentVideo')
@@ -13,6 +24,8 @@ router.get('/currentVideo', function (req, res, next) {
 router.get('/upload', (req, res, next) => {
   res.render('upload')
 })
+
+router.post("/upload",)
 
 
 module.exports = router;
