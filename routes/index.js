393f7f7f184d1fb4c91016256a97b5cf4f534813
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-03-15 01:08:40 +0530

    video streaming through local file

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,13 @@ var localStrategy = require('passport-local')
 passport.use(new localStrategy(userModel.authenticate()))
 const upload = require('./multer')
 const videoModel = require('./video')
+const fs = require('fs')
 
 
 
-router.get('/', isloggedIn, function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', isloggedIn, async function (req, res, next) {
+  const videos = await videoModel.find()
+  res.render('index', { title: 'Express', videos });
 });
 
 router.get('/login', (req, res, next) => {
@@ -24,8 +26,13 @@ router.get('/register', (req, res, next) => {
 
 
 
-router.get('/currentVideo', isloggedIn, function (req, res, next) {
-  res.render('currentVideo')
+router.get('/currentVideo/:videoId', isloggedIn, async function (req, res, next) {
+
+  const currentVideo = await videoModel.findOne({
+    _id: req.params.videoId
+  })
+
+  res.render('currentVideo', { currentVideo })
 })
 
 router.get('/upload', isloggedIn, (req, res, next) => {
@@ -81,7 +88,9 @@ router.post('/upload', isloggedIn, upload.single('vide_file'), async (req, res, 
 
   const newVideo = await videoModel.create({
     media: req.file.filename,
-    user: req.user._id
+    user: req.user._id,
+    title: req.body.title,
+    description: req.body.description
   })
 
   res.send(newVideo)
@@ -90,5 +99,70 @@ router.post('/upload', isloggedIn, upload.single('vide_file'), async (req, res, 
 
 /* ************** routes for video uploading ******************* */
 
+
+/* ************* Route for streaming ****************** */
+
+
+router.get('/stream/:fileName', isloggedIn, async (req, res, next) => {
+
+  const range = req.headers.range
+
+  const parts = range.replace('bytes=', "").split("-")
+  const start = parseInt(parts[ 0 ], 10)
+  let chunkSize = 1024 * 1024 * 4
+  let end = start + chunkSize - 1
+
+  const file = fs.statSync(`./public/video/${req.params.fileName}`)
+  const fileSize = file.size
+
+  if (end >= fileSize) {
+    end = fileSize - 1
+    chunkSize = start - end + 1
+  }
+
+  const head = {
+    "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": chunkSize - 1,
+    "Content-Type": "video/mp4"
+  }
+
+  res.writeHead(206, head)
+
+  fs.createReadStream(`./public/video/${req.params.fileName}`, {
+    start, end
+  }).pipe(res)
+
+
+  /*  const head = {
+     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+     'Accept-Ranges': 'bytes',
+     'Content-Length': chunksize,
+     'Content-Type': 'video/mp4',
+   };
+   res.writeHead(206, head); */
+
+
+
+  /*  console.log(range)
+   console.log(parts)
+   console.log(start) */
+
+})
+
+
+/* 
+
+0-
+
+*/
+
+/* ************* Route for streaming ****************** */
+
+
+/* 
+Read
+stream
+*/
 
 module.exports = router;
